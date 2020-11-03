7b225fac5d538de187815aed5d806dc148472ea5
henriB <henri.bretseau@gmail.com>
2020-11-03 10:57:52 +0100

    correction

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ function cursor(e) {
     mouseCursor.style.top =e.pageY +'px';
     mouseCursor.style.left =e.pageX +'px';
 }
+
+// CURSOR GROWING
+let navLinks = document.querySelectorAll('.nav-links li');
+console.log(navLinks);
+
+navLinks.forEach(link => {
+    link.addEventListener('mouseover', () =>{
+        mouseCursor.classList.add('link-grow');
+        link.classList.add('hovered-link');
+    })
+})
